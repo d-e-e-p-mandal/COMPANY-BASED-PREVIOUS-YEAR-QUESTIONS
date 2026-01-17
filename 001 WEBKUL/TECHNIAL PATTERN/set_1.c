6964220c217b4deacdcf1d690ba45fc3f0094892
“Deep <deepmandal9091@gmail.com>
2026-01-17 22:33:02 +0530

    add

---
+++ 001 WEBKUL/TECHNIAL PATTERN/set_1.c
@@ -0,0 +1,38 @@
+/*
+Input Value : Greater then Equal to 3 and Odd Number.
+N = 3
+  eeeeee     eeee***
+  e    e     e    *
+  e    e     e    e
+***    e     e    e
+ *     e     e    e
+
+N = 5                *****
+    eeeeeeee    eeeeee***
+    e      e    e      *
+    e      e    e      e
+    e      e    e      e
+    e      e    e      e
+*****      e    e      e
+ ***       e    e      e
+  *    
+
+N = 7 
+        N+3
+      eeeeeeeeee                
+      e        e                
+      e        e
+   N  e        e  N+2   eeeeee***
+      e        e        e
+      e        e        e
+  N   e        e        e
+*******        e        e
+ *****         e        e
+  ***                   e
+   *                    e
+   *                    e
+                        e
+
+
+
+*/
